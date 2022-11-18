18fa90d32b800ea988e453f58ca641affb6d77e3
MaureenMontano <maureenmontano149@gmail.com>
2022-11-18 15:50:42 -0600

    prueba

---
+++ contador-clicks/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import freeCodeCampLogo from './img/logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='freecodecamp-logo-contenedor'>
+        <img className='freecodecamp-logo' src={freeCodeCampLogo} alt='Logo de FreeCodeCamp' />
+      </div>
+    </div>
+  );
+}
+
+export default App;
